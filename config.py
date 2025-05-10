8d3c462c68b677ec5e9cbe03887bf0decb9e1dee
meet92656 <meetdarji742@gmail.com>
2025-05-10 19:02:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GayatrixAnand/SonaMusic1",
+    "https://github.com/meet92656/SonaMusic1",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THEDORESONA")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+L1-l-0w-MVdiYjU1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/wordy_Maze_7")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+DaUw1UcUA1BmOTFl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/7gsz5e.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/bmcgjp.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/yd51bx.jpg"
